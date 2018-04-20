21207b30b8047c9ccc2194246b0dd320c7243112
Theo Markettos <atm26@cl.cam.ac.uk>
2018-04-20 18:25:07 +0100

    Add a script to download and loopback mount Ubuntu images

---
+++ scripts/socfpga/fetch_ubuntu.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+UBUNTU_URL="http://cdimage.ubuntu.com/releases/16.04/release"
+UBUNTU_FILE="ubuntu-16.04.3-preinstalled-server-armhf+raspi2"
+
+# handy functions for driving losetup, based on
+# https://stackoverflow.com/a/39675265
+
+los() {
+  img="$1"
+  dev="$(sudo losetup --show -f -P "$img")"
+  for part in "$dev"?*; do
+    num=${part##${dev}p}
+    dst="mnt/$num"
+    echo "Found image partition $num, mounting $part at $dst"
+    sudo mkdir -p "$dst"
+    sudo mount "$part" "$dst"
+  done
+  loopdev="$dev"
+}
+
+losd() {
+  dev="$1"
+  for part in "$dev"?*; do
+    num=${part##${dev}p}
+    dst="mnt/$num"
+    echo "Found image partition $num, unmounting $part at $dst"
+    sudo umount "$part" "$dst"
+  done
+  sudo losetup -d "$dev"
+}
+
+
+wget -c $UBUNTU_URL/$UBUNTU_FILE.img.xz
+unxz $UBUNTU_FILE.img.xz
+los $UBUNTU_FILE.img
+echo $loopdev
+#losd $loopdev
