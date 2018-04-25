0e448f6733e0f576975142ea551331cb7288187f
Theo Markettos <atm26@cl.cam.ac.uk>
2018-04-25 17:47:02 +0100

    Rejig toplevel Ubuntu build script to turn all sections into functions. 2GB SD images work, 32GB SD images don't (looks like it can't find uboot)

--- scripts/socfpga/build_ubuntu_sdcard.sh
+++ scripts/socfpga/build_ubuntu_sdcard.sh
@@ -5,8 +5,8 @@ FPGA_PROJECT=$2
 FPGA_HANDOFF_DIR=hps_isw_handoff
 FPGA_BITFILE_RBF=$FPGA_DIR/output_files/$FPGA_PROJECT.rbf
 SD_IMAGE=sdimage.img
-ROOT_SIZE_MB=10000
-SD_SIZE_MB=12000
+ROOT_SIZE_MB=1500
+SD_SIZE_MB=2048
 echo $SCRIPT_PATH
 
 DTB=socfpga_arria10_socdk_sdmmc.dtb
@@ -14,26 +14,52 @@ DTB=socfpga_arria10_socdk_sdmmc.dtb
 SCRIPT_NAME=$(readlink -f "$0")
 SCRIPT_PATH=$(dirname "$SCRIPT_NAME")
 
-$SCRIPT_PATH/fetch_ubuntu.sh
+function ubuntu() {
+	$SCRIPT_PATH/fetch_ubuntu.sh
+	$SCRIPT_PATH/configure_networking.sh mnt/2/
+}
 
-$SCRIPT_PATH/build_linux.sh
+function kernel() {
+	$SCRIPT_PATH/build_linux.sh
+}
 
-cp -a linux-socfpga/vmlinux zImage
+function uboot() {
+	$SCRIPT_PATH/make_uboot.sh $FPGA_DIR/$FPGA_HANDOFF_DIR
+	cp -a bsp/uboot_w_dtb-mkpimage.bin .
+}
 
-$SCRIPT_PATH/make_uboot.sh $FPGA_DIR/$FPGA_HANDOFF_DIR
-cp -a bsp/uboot_w_dtb-mkpimage.bin .
+function devicetree() {
+	$SCRIPT_PATH/make_device_tree.sh $FPGA_DIR $FPGA_PROJECT.sopcinfo
 
-$SCRIPT_PATH/make_device_tree.sh $FPGA_DIR $FPGA_PROJECT.sopcinfo
+	cp -a $FPGA_DIR/$DTB $DTB	
+}
 
-cp -a $FPGA_DIR/$DTB $DTB	
-cp -a $FPGA_BITFILE_RBF socfpga.rbf
+function bitfile() {
+	cp -a $FPGA_BITFILE_RBF socfpga.rbf
+}
 
-echo "Building SD card image"
-sudo $SCRIPT_PATH/make_sdimage.py -f	\
-	-P uboot_w_dtb-mkpimage.bin,num=3,format=raw,size=10M,type=A2 \
-	-P mnt/2/*,num=2,format=ext3,size=${ROOT_SIZE_MB}M \
-	-P zImage,socfpga.rbf,socfpga_arria10_socdk_sdmmc.dtb,num=1,format=vfat,size=500M \
-	-s ${SD_SIZE_MB}M \
-	-n $SD_IMAGE
+function sdimage() {
 
-sudo umount mnt/1 mnt/2
+	echo "Building SD card image"
+	sudo $SCRIPT_PATH/make_sdimage.py -f	\
+		-P uboot_w_dtb-mkpimage.bin,num=3,format=raw,size=10M,type=A2 \
+		-P mnt/2/*,num=2,format=ext3,size=${ROOT_SIZE_MB}M \
+		-P zImage,socfpga.rbf,socfpga_arria10_socdk_sdmmc.dtb,num=1,format=vfat,size=500M \
+		-s ${SD_SIZE_MB}M \
+		-n $SD_IMAGE
+
+}
+
+
+function tidy() {
+	sudo umount mnt/1 mnt/2
+}
+
+
+ubuntu
+kernel
+uboot
+devicetree
+bitfile
+sdimage
+tidy
