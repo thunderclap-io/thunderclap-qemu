f390f95ee3aec94af87be90813042bbd9195f5ba
Theo Markettos <atm26@cl.cam.ac.uk>
2018-05-04 15:44:00 +0100

    Convert the bitfile

--- scripts/socfpga/make_bitfile.sh
+++ scripts/socfpga/make_bitfile.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/sh -e
 
 PATH=$1
 PROJECT=$2
 
 # assume we've already compiled Quartus project
-quartus_cpf $1/output_files/$2.sof $1/output_files/$2.rbf
+quartus_cpf -c $1/output_files/$2.sof $1/output_files/$2.rbf
