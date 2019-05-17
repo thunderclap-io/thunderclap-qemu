bda0b745f04be6fc7b19936d45d2c71fb7e6bcae
Theo Markettos <git+atm26@cl.cam.ac.uk>
2019-05-17 15:10:37 +0100

    Remove old 'scripts' folder A newer version can be found in https://github.com/thunderclap-io/thunderclap-ubuntu-sdcard

--- scripts/socfpga/configure_networking.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-INSTALL=$1
-CFG=$INSTALL/etc/network/interfaces.d/60-ipv6dns.cfg
-
-echo "iface eth0 inet6 auto" | sudo tee -a $CFG
-echo "        dns-nameservers 2001:4860:4860::8888 2001:4860:4860::8844" | sudo tee -a $CFG
--- scripts/socfpga/make_bitfile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -e
-
-TREE=$1
-PROJECT=$2
-
-# assume we've already compiled Quartus project
-quartus_cpf -c $TREE/output_files/$PROJECT.sof $TREE/output_files/$PROJECT.rbf
